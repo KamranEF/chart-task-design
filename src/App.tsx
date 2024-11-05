ff051bf2fedd8e701c6bd584c95ca3cd4bae2463
Eminov Kamran <eminov.kamran@wildberries.work>
2024-11-05 18:47:40 +0300

    feat: new

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import SkillGraph from './components/SkillGraph.tsx';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Компетенции и навыки</h1>
+            <SkillGraph />
+        </div>
+    );
+}
+
+export default App;
